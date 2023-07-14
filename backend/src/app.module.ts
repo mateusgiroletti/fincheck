078242e995876e35e939b1ec80da1fea96a3ac93
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-07-14 19:26:33 -0300

    adding repository

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule, DatabaseModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,16 +1,16 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { PrismaService } from 'src/database/prisma.service';
 import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(private readonly usersRepo: UsersRepository) {}
 
     async create(createUserDto: CreateUserDto) {
         const { name, email, password } = createUserDto;
 
-        const emailTaken = await this.prismaService.user.findUnique({
+        const emailTaken = await this.usersRepo.findUnique({
             where: { email },
             select: { id: true },
         });
@@ -21,10 +21,13 @@ export class UserService {
 
         const hashedPassword = await hash(password, 12);
 
-        const newUser = await this.prismaService.user.create({
+        const newUser = await this.usersRepo.create({
             data: { name, email, password: hashedPassword },
         });
 
-        return newUser;
+        return {
+            name: newUser.name,
+            email: newUser.email,
+        };
     }
 }
