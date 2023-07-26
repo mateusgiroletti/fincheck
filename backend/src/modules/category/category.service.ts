05b4d846596a47c6bf37c0c072880dc4642c0162
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-07-25 21:07:21 -0300

    insert find many category

--- backend/src/modules/category/category.service.ts
+++ backend/src/modules/category/category.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoryRepository } from 'src/shared/database/repositories/category.repositories';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+    constructor(private readonly categoryRepo: CategoryRepository) {}
 
-  findAll() {
-    return `This action returns all category`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+    findAll() {
+        return this.categoryRepo.findMany({
+            where: { userId: null },
+        });
+    }
 }
--- backend/src/shared/database/database.module.ts
+++ backend/src/shared/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from './repositories/user.repositories';
+import { CategoryRepository } from './repositories/category.repositories';
 
 @Global()
 @Module({
-    providers: [PrismaService, UserRepository],
-    exports: [UserRepository],
+    providers: [PrismaService, UserRepository, CategoryRepository],
+    exports: [UserRepository, CategoryRepository],
 })
 export class DatabaseModule {}
