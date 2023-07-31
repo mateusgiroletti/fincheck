1f2d17c39e40d0eaf778ce7c8c43cf0b9a9f0a90
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-07-31 17:38:47 -0300

    export validate category owner

--- backend/src/modules/category/category.module.ts
+++ backend/src/modules/category/category.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './services/category.service';
 import { CategoryController } from './category.controller';
+import { ValidateCategoryOwnershipService } from './services/validate-category-ownership.service';
 
 @Module({
     controllers: [CategoryController],
-    providers: [CategoryService],
+    providers: [CategoryService, ValidateCategoryOwnershipService],
+    exports: [ValidateCategoryOwnershipService],
 })
 export class CategoryModule {}
