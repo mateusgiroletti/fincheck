10ab04247d28fbd76b23a51d55b17a4ee70cac55
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-08-27 17:11:05 -0300

    create testing to userController

--- backend/src/modules/user/user.controller.spec.ts
+++ backend/src/modules/user/user.controller.spec.ts
@@ -1,20 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserRepository } from '../../shared/database/repositories/user.repositorie';
+import { PrismaService } from '../../shared/database/prisma.service';
 
 describe('UserController', () => {
-    let controller: UserController;
+    let userController: UserController;
+    let userService: UserService;
+
+    const userId = 'testUserId';
+    const user = { id: userId, name: 'Test User' };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UserController],
-            providers: [UserService],
+            providers: [
+                {
+                    provide: UserService,
+                    useValue: {
+                        getUserById: jest.fn().mockResolvedValue(user),
+                    },
+                },
+                UserRepository,
+                PrismaService,
+            ],
         }).compile();
 
-        controller = module.get<UserController>(UserController);
+        userController = module.get<UserController>(UserController);
+        userService = module.get<UserService>(UserService);
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(userController).toBeDefined();
+    });
+
+    describe('me', () => {
+        it('should return user', async () => {
+            // Mock the LoggedUserId decorator
+            const loggedUserIdDecorator = jest.fn().mockReturnValue(userId);
+
+            // Call the controller method
+            const result = await userController.me(loggedUserIdDecorator());
+
+            // Assert the result
+            expect(result).toEqual(user);
+            expect(loggedUserIdDecorator).toHaveBeenCalled();
+            expect(userService.getUserById).toHaveBeenCalledWith(userId);
+        });
     });
 });
