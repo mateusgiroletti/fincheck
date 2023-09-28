925f74f7d3979cabc419381c7e54859fb255ba11
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-09-27 22:04:05 -0300

    create AuthGuard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Router } from "./Router";
+
+
 
 export function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<h1>Login</h1>} />
-                <Route path="/register" element={<h1>register</h1>} />
-                <Route path="/" element={<h1>Dashboard</h1>} />
-            </Routes>
-        </BrowserRouter>
-    );
+    return <Router />;
 }
--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -1,5 +1,20 @@
-export function Router(){
-    return(
-        <h1></h1>
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { AuthGuard } from "./AuthGuard";
+
+export function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthGuard isPrivate={false} />} >
+                    <Route path="/login" element={<h1>Login</h1>} />
+                    <Route path="/register" element={<h1>register</h1>} />
+                </Route>
+                <Route element={<AuthGuard isPrivate={true} />} >
+                    <Route path="/" element={<h1>Dashboard</h1>} />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
     );
 }
