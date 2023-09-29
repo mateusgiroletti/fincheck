56165d11d89ac03b9aa8aba03375fb73cb9682ab
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-09-29 20:38:59 -0300

    create button component and register page

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Login() {
     return (
-        <div>
-            <header className="flex flex-col items-center gap-4">
+        <>
+            <header className="flex flex-col items-center gap-4 text-center">
                 <h1 className="text-2xl font-bold text-gray-900 tracking[-1px]">
                     Entre na sua conta
                 </h1>
@@ -23,14 +24,14 @@ export function Login() {
                 </p>
             </header>
 
-            <form className="w-full mt-[60px] flex flex-col gap-4">
+            <form className="mt-[60px] flex flex-col gap-4">
                 <Input name="email" type="email" placeholder="E-mail" />
                 <Input name="password" type="password" placeholder="Senha" />
 
-                <button type="submit" className="mt-2">
+                <Button type="submit" className="mt-2">
                     Entrar
-                </button>
+                </Button>
             </form>
-        </div>
+        </>
     );
 }
--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,5 +1,38 @@
-export function Register(){
-    return(
-        <h1>Register</h1>
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function Register() {
+    return (
+        <>
+            <header className="flex flex-col items-center gap-4 text-center">
+                <h1 className="text-2xl font-bold text-gray-900 tracking[-1px]">
+                    Crie sua conta
+                </h1>
+
+                <p className="space-x-2">
+                    <span className="text-gray-700 tracking-[-0.5px]">
+                        Já possui uma conta?
+                    </span>
+
+                    <Link
+                        to="/login"
+                        className="tracking-[-0.5px] font-medium text-teal-900"
+                    >
+                        Fazer Login
+                    </Link>
+                </p>
+            </header>
+
+            <form className="mt-[60px] flex flex-col gap-4">
+                <Input name="name"  placeholder="Nome" />
+                <Input type="email"name="te"  placeholder="E-mail" />
+                <Input name="password" type="password" placeholder="Senha" />
+
+                <Button type="submit" className="mt-2">
+                    Criar
+                </Button>
+            </form>
+        </>
     );
 }
