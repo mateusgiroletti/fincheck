ed6263e5b16fe4acdaab09a445d9cb65abafeaaf
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-09-28 21:26:13 -0300

    create AuthLayout

--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AuthGuard } from "./AuthGuard";
 
+import { Login } from "../view/pages/Login";
+import { Register } from "../view/pages/Register";
+import { Dashboard } from "../view/pages/Dashboard";
+import { AuthLayout } from "../view/layouts/AuthLayout";
+
 export function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<AuthGuard isPrivate={false} />} >
-                    <Route path="/login" element={<h1>Login</h1>} />
-                    <Route path="/register" element={<h1>register</h1>} />
+                    <Route element={<AuthLayout />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Route>
                 </Route>
                 <Route element={<AuthGuard isPrivate={true} />} >
-                    <Route path="/" element={<h1>Dashboard</h1>} />
+                    <Route path="/" element={<Dashboard />} />
                 </Route>
 
             </Routes>
