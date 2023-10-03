87e931c89c8a9eeb99da4895de5914005bdc1e3b
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-10-03 20:59:37 -0300

    Create validation to register page

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { useRegisterController } from "./useRegisterController";
 
 export function Register() {
+    const { errors, handleSubmit, register } = useRegisterController();
+
     return (
         <>
             <header className="flex flex-col items-center gap-4 text-center">
@@ -19,18 +22,37 @@ export function Register() {
                         to="/login"
                         className="tracking-[-0.5px] font-medium text-teal-900"
                     >
-                        Fazer Login
+                        Fazer login
                     </Link>
                 </p>
             </header>
 
-            <form className="mt-[60px] flex flex-col gap-4">
-                <Input name="name"  placeholder="Nome" />
-                <Input type="email"name="te"  placeholder="E-mail" />
-                <Input name="password" type="password" placeholder="Senha" />
+            <form
+                onSubmit={handleSubmit}
+                className="mt-[60px] flex flex-col gap-4"
+            >
+                <Input
+                    placeholder="Nome"
+                    error={errors.name?.message}
+                    {...register("name")}
+                />
+
+                <Input
+                    type="email"
+                    placeholder="E-mail"
+                    error={errors.email?.message}
+                    {...register("email")}
+                />
+
+                <Input
+                    type="password"
+                    placeholder="Senha"
+                    error={errors.password?.message}
+                    {...register("password")}
+                />
 
                 <Button type="submit" className="mt-2">
-                    Criar
+                    Criar conta
                 </Button>
             </form>
         </>
---
+++ frontend/src/view/pages/Register/useRegisterController.ts
@@ -0,0 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+    name: z.string().nonempty("Nome é obrigatório"),
+    email: z.string()
+        .nonempty("E-mail é obrigatório")
+        .email("Informe um e-mail válido"),
+    password: z.string()
+        .nonempty("Senha é obrigatória")
+        .min(8, "Senha deve conter pelo menos 8 dígitos"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useRegisterController() {
+    const {
+        handleSubmit: hookFormSubmit,
+        register,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(schema),
+    });
+
+    const handleSubmit = hookFormSubmit(async (data) => {
+        console.log(data);
+    });
+
+    return { register, errors, handleSubmit };
+}
