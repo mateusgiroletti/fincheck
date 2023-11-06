67878956ed3729c7961f6a85517ba215ae8ed04f
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-11-06 16:31:42 -0300

    style: changing bg color

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -8,7 +8,7 @@ export function UserMenu() {
     return (
         <DropdownMenu.Root>
             <DropdownMenu.Trigger>
-                <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+                <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100 cursor-pointer">
                     <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
                         {user?.name.slice(0, 2).toUpperCase()}
                     </span>
