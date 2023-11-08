d6a145828d2690a4416ac36a57e207e88831f19d
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-11-08 14:05:12 -0300

    style: cursor pointer on option values

--- frontend/src/view/components/Select.tsx
+++ frontend/src/view/components/Select.tsx
@@ -63,7 +63,7 @@ export function Select({ className, placeholder, options, error, onChange, value
                                     <RdxSelect.Item
                                         key={option.value}
                                         value={option.value}
-                                        className="p-2 text-gray-800 text-sm data-[state=checked]:font-bold outline-none data-[highlighted]:bg-gray-50 rounded-lg transition-colors"
+                                        className="p-2 text-gray-800 text-sm data-[state=checked]:font-bold outline-none data-[highlighted]:bg-gray-50 rounded-lg transition-colors cursor-pointer"
                                     >
                                         <RdxSelect.ItemText>{option.label}</RdxSelect.ItemText>
                                     </RdxSelect.Item>
