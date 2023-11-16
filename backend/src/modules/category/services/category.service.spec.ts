f30741347d42bc584ff4f530bbf2266f2d9c6217
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-11-16 17:16:33 -0300

    test: create test for category service

--- backend/src/modules/category/services/category.service.spec.ts
+++ backend/src/modules/category/services/category.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { CategoryRepository } from '../../../shared/database/repositories/category.repositorie';
+import { PrismaService } from '../../../shared/database/prisma.service';
+import { TransactionType } from '../../../modules/transaction/entities/Transaction';
 
 describe('CategoryService', () => {
-    let service: CategoryService;
+    let categoryService: CategoryService;
+    let categoryRepository: CategoryRepository;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [CategoryService],
+            providers: [CategoryService, CategoryRepository, PrismaService],
         }).compile();
 
-        service = module.get<CategoryService>(CategoryService);
+        categoryService = module.get<CategoryService>(CategoryService);
+        categoryRepository = module.get<CategoryRepository>(CategoryRepository);
     });
 
     it('should be defined', () => {
-        expect(service).toBeDefined();
+        expect(categoryService).toBeDefined();
+        expect(categoryRepository).toBeDefined();
+    });
+
+    describe('findAll', () => {
+        it('should return all categories where userId is null', async () => {
+            const mockCategory = [
+                {
+                    id: '1',
+                    userId: null,
+                    name: 'cat',
+                    icon: 'icon',
+                    type: TransactionType.INCOME,
+                    isCustom: false,
+                },
+            ];
+
+            jest.spyOn(categoryRepository, 'findMany').mockResolvedValue(
+                mockCategory,
+            );
+
+            const category = await categoryService.findAll();
+
+            // Assert that the service returns the expected user
+            expect(category).toEqual(mockCategory);
+
+            // Assert that the findMany method of CategoryRepository was called with the correct parameters
+            expect(categoryRepository.findMany).toHaveBeenCalledWith({
+                where: { userId: null },
+            });
+        });
     });
 });
