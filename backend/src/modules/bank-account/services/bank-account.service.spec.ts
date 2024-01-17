c12c61f0b2f2015a71d481844959fadde1ba669e
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-01-17 11:35:57 -0300

    test: create bank-account-service

--- backend/src/modules/bank-account/services/bank-account.service.spec.ts
+++ backend/src/modules/bank-account/services/bank-account.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BankAccountService } from './bank-account.service';
+import { BankAccountRepository } from '../../../shared/database/repositories/bank-account.repositorie';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
+import { PrismaService } from '../../../shared/database/prisma.service';
 
 describe('BankAccountService', () => {
-    let service: BankAccountService;
+    let bankAccountService: BankAccountService;
+    let bankAccountRepository: BankAccountRepository;
+    let validateBankAccountOwnershipService: ValidateBankAccountOwnershipService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [BankAccountService],
+            providers: [BankAccountService, BankAccountRepository, ValidateBankAccountOwnershipService, PrismaService],
         }).compile();
 
-        service = module.get<BankAccountService>(BankAccountService);
+        bankAccountService = module.get<BankAccountService>(BankAccountService);
+        bankAccountRepository = module.get<BankAccountRepository>(BankAccountRepository);
+        validateBankAccountOwnershipService = module.get<ValidateBankAccountOwnershipService>(ValidateBankAccountOwnershipService);
     });
 
     it('should be defined', () => {
-        expect(service).toBeDefined();
+        expect(bankAccountService).toBeDefined();
+        expect(bankAccountRepository).toBeDefined();
+        expect(validateBankAccountOwnershipService).toBeDefined();
     });
+
 });
