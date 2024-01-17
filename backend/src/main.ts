db37552292c88ed098aaa421b078d8852c5b6cfb
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-01-17 15:34:18 -0300

    docs: config swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
     app.enableCors({
         origin: '*',
     });
+
+    const config = new DocumentBuilder()
+        .setTitle('Fincheck API')
+        .setDescription('The Fincheck API description')
+        .setVersion('1.0')
+        .build();
+        
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
     await app.listen(3000);
 }
