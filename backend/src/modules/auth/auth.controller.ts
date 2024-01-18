39f8b22f5ffe3559b384065628055bd58f53f3ae
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-01-18 14:21:33 -0300

    docs: auth endpoint

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -3,7 +3,13 @@ import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin';
 import { SignupDto } from './dto/signup';
 import { IsPublic } from '../../shared/decorators/isPublic';
-import { ApiTags } from '@nestjs/swagger';
+import {
+    ApiConflictResponse,
+    ApiOperation,
+    ApiResponse,
+    ApiTags,
+    ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @IsPublic()
 @Controller('auth')
@@ -12,11 +18,43 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('signin')
+    @ApiOperation({ summary: 'User authentication' })
+    @ApiResponse({
+        status: 201,
+        description: 'Provides an authentication token',
+        schema: {
+            type: 'object',
+            properties: {
+                token: { type: 'string' },
+            },
+        },
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'Invalid credentials.',
+        type: ApiUnauthorizedResponse,
+    })
     signin(@Body() signinDto: SigninDto) {
         return this.authService.signin(signinDto);
     }
 
     @Post('signup')
+    @ApiOperation({ summary: 'Create the user and authenticate' })
+    @ApiResponse({
+        status: 201,
+        description: 'Provides an authentication token',
+        schema: {
+            type: 'object',
+            properties: {
+                token: { type: 'string' },
+            },
+        },
+    })
+    @ApiResponse({
+        status: 409,
+        description: 'This email is already in use',
+        type: ApiConflictResponse(),
+    })
     create(@Body() signupDto: SignupDto) {
         return this.authService.signup(signupDto);
     }
